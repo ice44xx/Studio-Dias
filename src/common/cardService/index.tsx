eb7762f0b90c97d35d9df76032ada1315cf57745
Nathan Sant Helena <ice44xx@gmail.com>
2023-10-12 19:05:17 -0300

    fix layout

--- src/common/cardService/index.tsx
+++ src/common/cardService/index.tsx
@@ -15,6 +15,7 @@ const CardService: React.FC<Props> = ({ name, beforeImg, afterImg }) => {
         <div className={styles.circle} onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
           <img src='/assets/circle.webp' className={styles.circle_img} />
           <div className={styles.container_img}>
+            <p className={styles.after}>Click aqui!</p>
             <img src={isHover ? afterImg : beforeImg} alt={name} className={styles.img} />
           </div>
         </div>
