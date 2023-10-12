2604fdabbef10108f62baed32cc79c7e5a277673
Nathan Sant Helena <ice44xx@gmail.com>
2023-10-12 12:46:13 -0300

    feat: insert shadow in containers

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,10 +2,7 @@ import styles from './styles.module.scss';
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <a href='/privacy.html'>Privacy Policy</a> - 2022 | Prod por{' '}
-      <a href=' http://api.whatsapp.com/send?1=pt_BR&phone=5551985981382' target='_blank'>
-        NSHCODE <i className='fa-regular fa-heart'></i>
-      </a>
+      <a href='/privacy.html'>Privacy Policy</a> - 2022 | Prod por NSHCODE <i className='fa-regular fa-heart'></i>
     </div>
   );
 };
